455020381ad758d7116c1f4bd9e0fc69c75877d1
Kevin Arutyunyan <d3d9@riseup.net>
2021-07-27 17:49:34 +0200

    NINA API

--- service/run.sh
+++ service/run.sh
@@ -29,6 +29,9 @@ fi
 
 echo "selection: $selection"
 case $selection in
+feuerwache)
+  ./dm_tomatrixled.py -s de:05914:2216 -b$brightness -er0 -w16 -l8 -f8 --led-slowdown-gpio 2 --update-steps 330 --sleep-interval 0.025 --test-ext https://d3d9.xyz:8008/data?id=feuerwache --hst-colors --platform-width 13 --local-deps "./feuerwache.csv" --nina-url "https://warnung.bund.de/api31/dashboard/" --nina-ags "059140000000" --nina-ignore-msgType "Update" --nina-ignore-id "lhp.HOCHWASSERZENTRALEN.DE.NW" --nina-ignore-id "mow.DE-NW-HA-SE087-20210723-87-000" # --no-rt-msg -1
+  ;;
 emilienplatz)
   ./dm_tomatrixled.py -s de:05914:2075:0:1 -b$brightness -er1 -l8 -f8 --update-steps 330 --ignore-infoid 45828_HST --ignore-infoid 54354_HST
   ;;
